ab39aa76984deb9fed05c7ea6b5907d84909fd6c
Igor Puzhakov <igor.p619@outlook.com>
2023-04-20 13:00:20 +0300

    refactoring code n component.tsx

--- cypress/support/component.tsx
+++ cypress/support/component.tsx
@@ -1,5 +1,6 @@
+import React from "react";
 import { mount, MountOptions, MountReturn } from "cypress/react";
-import { MemoryRouter, MemoryRouterProps } from "react-router-dom";
+import { MemoryRouterProps } from "react-router-dom";
 import "@testing-library/cypress/add-commands";
 import { wrapper as Wrapper } from "../../src/mocks/wrapper";
 import "./commands";
@@ -10,13 +11,7 @@ let isRunning = false;
 
 //cypress/supports/index.js
 Cypress.on("test:before:run:async", async () => {
-  if (isRunning) {
-    console.log("MSW is already running.");
-  }
-
-  //if MSW wasnt started by the app, Cypress needs to start it
   if (!isRunning) {
-    console.log("MSW has not been started. Starting now.");
     await worker.start();
     isRunning = true;
   }
@@ -41,16 +36,7 @@ declare global {
 Cypress.Commands.add("mount", (component, options = {}) => {
   const { routerProps, ...mountOptions } = options;
 
-  const wrapped = (
-    <Wrapper
-      routerProps={{
-        ...routerProps,
-        initialEntries: ["/movie/502356?fromPage=nowPlaying"],
-      }}
-    >
-      {component}
-    </Wrapper>
-  );
+  const wrapped = <Wrapper routerProps={routerProps}>{component}</Wrapper>;
 
   return mount(wrapped, mountOptions);
 });
